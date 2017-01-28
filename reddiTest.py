a1e266168d0d1b90b99d34396a200e06be0efc51
Greg Starr <gregstarr2@gmail.com>
2017-01-28 16:22:50 -0500

    initial test

--- reddiTest.py
+++ reddiTest.py
@@ -1,8 +1,26 @@
-import praw
+from praw import Reddit
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-reddit = praw.Reddit(client_id='N5kDJcWEEK7BDw', client_secret='teB5Ykrjyh_ygkeGKk8mOrAyBRU', password='fuckfuck77', user_agent='whatever you want this to say?', username='gregstarr')
+subreddit_name = 'science'
 
-for submission in reddit.subreddit('programmingcirclejerk').hot(limit=25):
-	print(submission.title)
-	for comment in submission.comments:
-		print(comment)
+reddit = Reddit(client_id='N5kDJcWEEK7BDw', 
+                client_secret='teB5Ykrjyh_ygkeGKk8mOrAyBRU', 
+                password='fuckfuck77', 
+                user_agent='whatever you want this to say?', 
+                username='gregstarr')
+
+subreddit = reddit.subreddit(subreddit_name).hot()
+top_submission = reddit.submission(subreddit.next())
+
+comments = []
+
+top_submission.comments.replace_more(limit=0)
+for top_level_comment in top_submission.comments:
+    comments.append(top_level_comment.body)
+    
+sid = SentimentIntensityAnalyzer()
+
+for comment in comments:
+    print(comment)
+    print(sid.polarity_scores(comment))
+    print()
