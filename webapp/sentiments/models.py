2bc5a4fcee5f86d04c19520c5a68ad6f99ab01f1
Tony Bushara <tonybushara@gmail.com>
2017-01-28 20:01:32 -0500

    Updated models for the webapp

--- webapp/sentiments/models.py
+++ webapp/sentiments/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class SubReddit(models.Model):
+    subreddit_name = models.CharField(max_length=20)
+    subscribers = models.IntegerField(null=True)
+    positivity_index = models.IntegerField(null=True)
+    negativity_index = models.IntegerField(null=True)
+    neutrality_index = models.IntegerField(null=True)
+    num_positive_comments = models.IntegerField(null=True)
+    num_negative_comments = models.IntegerField(null=True)
+    num_neutral_comments = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.subreddit_name
